9cab3545ed6471867d56a4658e8b41ca9b0f1895
David Kim <dkhw90@gmail.com>
2025-01-21 21:16:20 +0100

    Envelope json responses

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -8,12 +8,12 @@ import (
 // (i.e. start with a capital letter), which is necessary for them to be visible to Go’s encoding/json package.
 // Any fields which aren’t exported won’t be included when encoding a struct to JSON.
 type Movie struct {
-	ID        int64     `json:"id"`         // Unique integer ID for the movie
-	CreatedAt time.Time `json:"created_at"` // Timestamp for when the movie is added to our database
-	Title     string    `json:"title"`      // Movie title
-	Year      int32     `json:"year"`       // Movie release year
-	Runtime   int32     `json:"runtime"`    // Movie runtime (in minutes)
-	Genres    []string  `json:"genres"`     // Slice of genres for the movie (romance, comedy, etc.)
-	Version   int32     `json:"version"`    // The version number starts at 1 and will be incremented each
+	ID        int64     `json:"id"`                // Unique integer ID for the movie
+	CreatedAt time.Time `json:"-"`                 // Timestamp for when the movie is added to our database
+	Title     string    `json:"title"`             // Movie title
+	Year      int32     `json:"year,omitempty"`    // Movie release year
+	Runtime   int32     `json:"runtime,omitempty"` // Movie runtime (in minutes)
+	Genres    []string  `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
+	Version   int32     `json:"version"`           // The version number starts at 1 and will be incremented each
 	// time the movie information is updated
 }
