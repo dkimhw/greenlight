6a87d1880a5f29dcaceb8822067312120e01727e
David Kim <dkhw90@gmail.com>
2025-03-04 22:45:07 +0100

    Add error handling for server panic; resource not found; 404

---
+++ cmd/api/middleware.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// deferred function always runs in the event of panic as Go unwinds the stack
+		defer func() {
+			// check if there has been a panic
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close") // triggers Go to close current connection after res is sent
+				// The value returned by recover() has the type any, so we use
+				// fmt.Errorf() to normalize it into an error and call our
+				// serverErrorResponse() helper. In turn, this will log the error using
+				// our custom Logger type at the ERROR level and send the client a 500
+				// Internal Server Error response.
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,13 +10,18 @@ import (
 // Keeps main function clean
 // Easier to access test code by initializing an application instance and calling the routes() method on it.
 func (app *application) routes() http.Handler {
-	// Initialize a new httprouter router instance
 	router := httprouter.New()
+
+	// Convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
+	// Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	// Register relevant methods, url patterns, and handler functions
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
